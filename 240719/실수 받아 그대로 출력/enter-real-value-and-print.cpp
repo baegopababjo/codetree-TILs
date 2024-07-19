de26b1f0334ecbe453dbe3d7ad811039511209c8
Dasu <ekgns1106@naver.com>
2024-07-19 16:14:26 +0530

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 0.0;
+    cin>>a;
+    cout<<fixed;
+    cout.precision(2);
+    cout<<a;
+    return 0;
+}
