087b68c08e6b3b0006a1b82a793073bd85207498
Dasu <ekgns1106@naver.com>
2024-07-19 16:22:16 +0530

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double fit = 30.48;
+    double a;
+    cin>>a;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<a*fit;
+    return 0;
+}
