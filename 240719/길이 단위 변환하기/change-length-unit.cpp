3c8975a1286803d19da58b771f662014a211bfd9
Dasu <ekgns1106@naver.com>
2024-07-19 15:25:40 +0530

    Title: 길이 단위 변환하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double fit = 30.48;
+    double mi = 160934.0;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<"9.2ft = "<<fit*9.2<<"cm"<<endl;
+    cout<<"1.3mi = "<<mi*1.3<<"cm"<<endl;
+    return 0;
+}
