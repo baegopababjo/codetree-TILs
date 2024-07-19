d34e6418b22196f1d756de5b0b0ae6522c2841cd
Dasu <ekgns1106@naver.com>
2024-07-19 17:15:23 +0530

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    char m;
+    cin>>a>>m>>b>>m>>c;
+    cout<<"0"<<a<<"-"<<c<<m<<b;
+    return 0;
+}
