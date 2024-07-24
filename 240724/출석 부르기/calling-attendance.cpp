6eee4977fe0dfd8c7c729edf7b8e91ae912034d4
Dasu <ekgns1106@naver.com>
2024-07-24 09:21:56 +0530

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i;
+    cin>>i;
+
+    if(i==1){
+        cout<<"John";
+    }else if(i==2){
+        cout<<"Tom";
+    }else if(i==3){
+        cout<<"Paul";
+    }else{
+        cout<<"Vacancy";
+    }
+    return 0;
+}
